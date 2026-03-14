94bcebb07e8b9517af53b95f64def0db684d34dd
medazizzl <zghalmalak10@gmail.com>
2026-03-14 15:33:24 +0100

    fix service worker cache update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
-
 export default defineConfig({
   plugins: [
     react(),
@@ -26,6 +25,8 @@ export default defineConfig({
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
         navigateFallback: '/index.html',
+        skipWaiting: true,
+        clientsClaim: true,
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/cifpwjdhfiyidvympecs\.supabase\.co\/.*/i,
